7fea4e12255c5a49b32b912469f809ce0a025f34
fazilmahesania <43927517+fazilmahesania@users.noreply.github.com>
2023-08-11 15:23:30 -0400

    Completed Template Driven Forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { UserSettingsFormComponent } from './user-settings-form/user-settings-form.component';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TimepickerModule } from 'ngx-bootstrap/timepicker';
+import { RatingModule } from 'ngx-bootstrap/rating';
+
+
 
 @NgModule({
   declarations: [
@@ -12,7 +20,13 @@ import { UserSettingsFormComponent } from './user-settings-form/user-settings-fo
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    ButtonsModule.forRoot(),
+    BsDatepickerModule.forRoot(),
+    BrowserAnimationsModule,
+    TimepickerModule.forRoot(),
+    RatingModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UserSettings } from '../data/user-settings';
+import { NgForm, NgModel } from '@angular/forms';
+import { DataService } from '../data/data.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-user-settings-form',
   templateUrl: './user-settings-form.component.html',
   styleUrls: ['./user-settings-form.component.css']
 })
-export class UserSettingsFormComponent {
-  originalUserSettings: UserSettings ={
+export class UserSettingsFormComponent implements OnInit {
+  originalUserSettings: UserSettings = {
     name: 'Milton',
     emailOffers: true,
     interfaceStyle: 'dark',
@@ -15,5 +18,53 @@ export class UserSettingsFormComponent {
     notes: 'Here are some notes...'
   };
 
-  userSettings : UserSettings = { ...this.originalUserSettings};
+  singleModel = "On";
+  startDate: Date | undefined;
+  startTime: Date = new Date();
+  maxRating = 10;
+  userRating = 0;
+  isReadonly = false;
+  userSettings: UserSettings = { ...this.originalUserSettings };
+  postError = false;
+  postErrorMessage = '';
+  subscriptionTypes: Observable<string[]> = new Observable<string[]>();
+
+
+  constructor(private dataService: DataService) {
+  }
+
+  ngOnInit() {
+    this.subscriptionTypes = this.dataService.getSubscriptionTypes();
+    this.startDate = new Date();
+  }
+
+
+
+  onBlur(field: NgModel) {
+    console.log('in onBlur: ', field.valid);
+  }
+
+  onHttpError(errorResponse: any){
+    console.log('error: ', errorResponse);
+    this.postError = true;
+    this.postErrorMessage = errorResponse.error.errorMessage;
+  }
+
+  onSubmit(form: NgForm) {
+    console.log('in onSubmit: ', form.value);
+
+  //   if(form.valid){
+  //   this.dataService.postUserSettingsForm(this.userSettings).subscribe({
+  //     next: result => { console.log('success: ', result) },
+  //     error: error => { this.onHttpError(error); }
+  //   }
+  //   );
+  // }
+  // else {
+  //   this.postError = true;
+  //   this.postErrorMessage = "Please fix the above errors";
+  // }
+
+
+  }
 }
